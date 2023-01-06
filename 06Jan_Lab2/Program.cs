e47621db27a64404695fce14c8306e1650717d50
Adil Heybetov <heybetov1998@gmail.com>
2023-01-07 02:35:53 +0400

    added lab tasks

---
+++ 06Jan_Lab2/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace _06Jan_Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 11 - Rəşad və Aydan yarışırlar. Jüri onların hər birinə 3 fərqli idman növündə 1-dən 100-ə qədər olmaqla qiymət verir. Hər idman növünə görə onların xalını müqayisə edin və həmin kateqoriya üzrə hansının xalı çoxdursa, onun dərəcəsini 1 vahid artırın. Əgər ikisinin də xalı bərabərdirsə, heç birinin dərəcəsini artırmayın. Bu dərəcələri ekrana çıxarın.
+            //int[] rashad = new int[3];
+            //int[] aydan = new int[3];
+
+            //int rashadDegree = 0;
+            //int aydanDegree = 0;
+
+            //Console.WriteLine("Reshadin juri qiymetlerini daxil edin: ");
+            //for (int i = 0; i < rashad.Length; i++)
+            //{
+            //    rashad[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("\nAydanin juri qiymetlerini daxil edin: ");
+            //for (int i = 0; i < aydan.Length; i++)
+            //{
+            //    aydan[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    if (aydan[i] > rashad[i]) aydanDegree++;
+            //    if (aydan[i] < rashad[i]) rashadDegree++;
+            //}
+
+            //Console.WriteLine($"\nRashadin derecesi: {rashadDegree}\nAydanin derecesi: {aydanDegree}");
+            #endregion
+            #region Task 13 - İki eyni ölçülü matrisin cəmini tapan proqram yazın
+            //Console.Write("Matrislerin setr sayini daxil edin: ");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.Write("Matrislerin sutun sayini daxil edin: ");
+            //int m = int.Parse(Console.ReadLine());
+
+            //int[,] arr1 = new int[n, m];
+            //int[,] arr2 = new int[n, m];
+            //int[,] arr3 = new int[n, m];
+
+            //Console.WriteLine("\nBirinci matris:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        Console.Write($"arr1[{i},{j}] = ");
+            //        arr1[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            //Console.WriteLine("\nIkinci matris:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        Console.Write($"arr2[{i},{j}] = ");
+            //        arr2[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        arr3[i, j] = arr1[i, j] + arr2[i, j];
+            //    }
+            //}
+
+            //Console.WriteLine("\nNetice:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        Console.Write($"{arr3[i, j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+        }
+    }
+}
